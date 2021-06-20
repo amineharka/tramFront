67b71adf698c9128d7eb8d43a91e1acc0b0c1fb0
Hicham-Essousy <hicham.es-sousy@edu.uiz.ac.ma>
2021-06-20 15:46:17 +0200

    last changes

--- src/app/utilisation/dashbord/dashbord.component.ts
+++ src/app/utilisation/dashbord/dashbord.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashbordComponent implements OnInit {
 
+  generateTicketRequest : any ={nomber : "",ligne : ""};
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  generateTicket(){
+
+  }
+
+  public onOpenModal()
+  {
+   
+   document.getElementById("overlay").style.visibility="visible";
+   document.getElementById("overlay").style.opacity="1";
+   document.getElementById("modal").style.visibility="visible";
+   document.getElementById("modal").style.top="50%";
+  }
+ 
+  public onCloseModal()
+  {
+   
+   document.getElementById("overlay").style.visibility="hidden";
+   document.getElementById("overlay").style.opacity="0";
+   document.getElementById("modal").style.visibility="hidden";
+   document.getElementById("modal").style.top="-50%";
   }
 
 }
